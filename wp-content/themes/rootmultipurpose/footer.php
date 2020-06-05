fc916e7e3c0a1a23ba5b68da4fe973dfea8facba
Mladen Živojinović <mladenzivojinovic96@gmail.com>
2020-06-05 14:20:08 +0200

    Create my theme

---
+++ wp-content/themes/rootmultipurpose/footer.php
@@ -0,0 +1,50 @@
+<div class="footer">
+        <div class="container">
+          <div class="row">
+            <div class="col-sm-5">
+              <div class="ft-left">
+                <div class="footer-img">
+                   <img src="<?php echo get_template_directory_uri().'/icons/pi-logo.png';?>" width="30" alt="Logo">
+                </div>
+                <p>Cu qui probo malorum saperet. Ne admodum apeirian iracundia usu, eam cu agam ludus,
+                  eum munere accusam molestie ut. Alienum percipitur ne est, pri quando iriure ad. </p>
+                <p>&copy; 2017 YDirection Themes by IOThemes</p>
+              </div>
+            </div>
+            <div class="col-sm-4 col-sm-offset-3">
+              <div class="ft-right">
+                <ul>
+                  <li><a href="#">About Us</a></li>
+                  <li><a href="#">Product Details</a></li>
+                  <li><a href="#">Pricing</a></li>
+                  <li><a href="#">Privacy policy</a></li>
+                  <li><a href="#">User Agreement</a></li>
+                  <li><a href="#">Contact</a></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+
+      </div> <!-- Main -->
+    </div><!-- Wrapper -->
+
+
+    <!-- Jquery and Js Plugins -->
+    <script type="text/javascript" src="assets/js/jquery-2.1.1.js"></script>
+    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/jquery.validate.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins.js"></script>
+    <script type="text/javascript" src="assets/js/validator.js"></script>
+    <script type="text/javascript" src="assets/js/custom.js"></script>
+
+    <script src="<?php echo get_template_directory_uri().'/js/jquery-2.1.1.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/bootstrap.min.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/jquery.validate.min.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/plugins.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/validator.js'; ?>" type="text/javascript"></script>
+    <script src="<?php echo get_template_directory_uri().'/js/custom.js'; ?>" type="text/javascript"></script>
+  </body>
+</html>
